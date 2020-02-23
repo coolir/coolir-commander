d73e076eef2a5ec968a8706e0e64c9674e570dcd
Dan Lynch <pyramation@gmail.com>
2020-02-23 17:33:04 -0500

    first commit

---
+++ packages/coolir-commander-utils/CHANGELOG.md
@@ -0,0 +1,40 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+
+## [0.1.6](https://github.com/pyramation/mania/compare/mania-utils@0.1.5...mania-utils@0.1.6) (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
+
+
+
+
+
+## [0.1.5](https://github.com/pyramation/mania/compare/mania-utils@0.1.4...mania-utils@0.1.5) (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
+
+
+
+
+
+## 0.1.4 (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
+
+
+
+
+
+## [0.1.3](https://github.com/pyramation/mania/compare/mania-utils@0.1.2...mania-utils@0.1.3) (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
+
+## [0.1.2](https://github.com/pyramation/mania/compare/mania-utils@0.1.1...mania-utils@0.1.2) (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
+
+## 0.1.1 (2020-02-23)
+
+**Note:** Version bump only for package mania-utils
---
+++ packages/coolir-commander/src/index.js
@@ -0,0 +1,66 @@
+import { prompt } from '@pyramation/prompt';
+import { makeAutocompleteFunctionWithInput } from 'mania-utils';
+
+export class Prompt {
+  constructor(cmds) {
+    this.cmds = cmds;
+  }
+  resolveAlias(cmd) {
+    const aliases = Object.keys(this.cmds).reduce((m, v) => {
+      m[v] = this.cmds[v].aliases;
+      return m;
+    }, {});
+    Object.keys(aliases).forEach(aliasCmd => {
+      if (
+        aliases[aliasCmd] &&
+        aliases[aliasCmd].length &&
+        aliases[aliasCmd].includes(cmd)
+      ) {
+        cmd = aliasCmd;
+      }
+    });
+    return cmd;
+  }
+
+  async getCommandName(argv) {
+    const searchCmds = makeAutocompleteFunctionWithInput(
+      Object.keys(this.cmds)
+    );
+    const cmdQuestion = [
+      {
+        _: true,
+        type: 'autocomplete',
+        name: 'cmd',
+        message: 'what do you want to create?',
+        source: searchCmds,
+      },
+    ];
+    let { cmd } = await prompt(cmdQuestion, argv);
+    if (!this.cmds.hasOwnProperty(cmd)) {
+      cmd = this.resolveAlias(cmd);
+      if (!this.cmds.hasOwnProperty(cmd)) {
+        console.error(`${cmd} does not exist!`);
+        return process.exit(0);
+      }
+    }
+
+    return cmd;
+  }
+
+  async getFunction(argv) {
+    const cmd = await this.getCommandName(argv);
+    const questions = this.cmds[cmd].questions || [];
+    const fn = this.cmds[cmd];
+    return async () => {
+      const results = await prompt(questions, argv);
+      await fn(results);
+    };
+  }
+}
+
+export const cli = async cmds => {
+  const argv = process.argv.slice(2);
+  const prompter = new Prompt(cmds);
+  const fn = await prompter.getFunction(argv);
+  const result = await fn();
+};
